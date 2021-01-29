43b9be2b055f2d1404a399f42287e1d8f4b83bb9
Kevin Lyons <zinckev@hotmail.com>
2021-01-28 16:06:38 -0800

    CRUD boiler plate added for the different tables

---
+++ routes/business_api_routes.js
@@ -0,0 +1,58 @@
+// requires the model folder
+
+const db = require("../models/business_model");
+
+module.exports = function (app) {
+  // create route for adding to the table business in the db:
+  app.post("/api/business", async (req, res) => {
+    const data = await db.Business.create(req.body);
+    res
+      .status(200)
+      .json(data)
+      .catch((err) => {
+        res.status(500);
+        console.err(err);
+        throw err;
+      });
+  });
+  // get route for reading the whole table business in the db:
+  app.get("/api/business", async (req, res) => {
+    const data = await db.Business.findall();
+    res
+      .status(200)
+      .json(data)
+      .catch((err) => {
+        res.status(500);
+        console.err(err);
+        throw err;
+      });
+  });
+
+  // update route for updating the info in the table business in the db: WIP
+  app.put("/api/business/:id", async (req, res) => {
+    const data = await db.Business.update(req.body, {
+      where: { id: req.body.id },
+    });
+    res
+      .status(200)
+      .json(data)
+      .catch((err) => {
+        res.status(500);
+        console.err(err);
+        throw err;
+      });
+  });
+
+  // delete route for deleting the info in the table business in the db:
+  app.put("/api/business/:id", async (req, res) => {
+    const data = await db.Business.destroy({ where: { id: req.params.id } });
+    res
+      .status(200)
+      .json(data)
+      .catch((err) => {
+        res.status(500);
+        console.err(err);
+        throw err;
+      });
+  });
+};
---
+++ routes/customer_api_route.js
@@ -0,0 +1,58 @@
+// requires the model folder
+
+const db = require("../models/business_model");
+
+module.exports = function (app) {
+  // create route for adding to the table business in the db:
+  app.post("/api/customer", async (req, res) => {
+    const data = await db.Customer.create(req.body);
+    res
+      .status(200)
+      .json(data)
+      .catch((err) => {
+        res.status(500);
+        console.err(err);
+        throw err;
+      });
+  });
+  // get route for reading the whole table Customer in the db:
+  app.get("/api/customer", async (req, res) => {
+    const data = await db.Customer.findall();
+    res
+      .status(200)
+      .json(data)
+      .catch((err) => {
+        res.status(500);
+        console.err(err);
+        throw err;
+      });
+  });
+
+  // update route for updating the info in the table Customer in the db: WIP
+  app.put("/api/customer/:id", async (req, res) => {
+    const data = await db.Customer.update(req.body, {
+      where: { id: req.body.id },
+    });
+    res
+      .status(200)
+      .json(data)
+      .catch((err) => {
+        res.status(500);
+        console.err(err);
+        throw err;
+      });
+  });
+
+  // delete route for deleting the info in the table Customer in the db:
+  app.put("/api/customer/:id", async (req, res) => {
+    const data = await db.Customer.destroy({ where: { id: req.params.id } });
+    res
+      .status(200)
+      .json(data)
+      .catch((err) => {
+        res.status(500);
+        console.err(err);
+        throw err;
+      });
+  });
+};
