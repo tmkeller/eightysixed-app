1d1d7f1345b5fc910c3d6c2ea63b92f91f4df078
Tim Keller <timothy.m.keller@gmail.com>
2021-02-01 12:23:00 -0800

    Removed business-main route from api-routes.js. Updated business-main/:id route with async statements and data from the businesses database to get business info on the page. Added the necessary code to the business-main template to implement that change.

--- routes/backend-get-routes.js
+++ routes/backend-get-routes.js
@@ -2,32 +2,42 @@ const db = require("../models");
 
 module.exports = function(app){
 
-    app.get("/business-main/:id",function( req, res ) {
-        db.Customer.findAll({
-            where:{
-                BusinessId: req.params.id
-            }
-        }).then(data=>{
-            if ( !req.session.business ) {
-                res.status( 401 ).send( "You must log in first." );
-                res.render( "/" );
-            } else {
-                const jsonData = data.map(obj=> {return obj.toJSON()})
-                const hbsObj = {
-                    customers: jsonData,
-                    user: ( req.session.user || req.session.business )
+    app.get("/business-main/:id", async function( req, res ) {
+        if ( !req.session.business ) {
+            res.status( 401 ).send( "You must log in first." );
+            res.render( "/" );
+        } else {
+            const customers = await db.Customer.findAll({
+                where:{
+                    BusinessId: req.params.id
                 }
-                console.log(data)
-    
-                
-                res.render('business-main', hbsObj);
-            }
-            
-        })
+            }).catch( err => {
+                res.status( 500 ).json( err );
+            });
+            const business = await db.Business.findOne({
+                where: {
+                    id: req.params.id
+                }
+            }).catch( err => {
+                res.status( 500 ).json( err );
+            });
 
+            const jsonData = customers.map(( obj ) => {
+                let newObj = obj.toJSON();
+                // Placeholder code for the star width.
+                newObj.star_width = Math.floor((3/5) * 187) + "px";
+                return newObj;
+            });
+            const hbsObj = await {
+                businessData: business.toJSON(),
+                customers: jsonData,
+                // This is necessary any time you're rendering a page
+                // where the user should be logged in. Looks like this for customers:
+                // customer: req.session.customer
+                business: req.session.business
+            };
+            console.log( "hbsObj", hbsObj );
+            res.render('business-main', hbsObj);
+        }
     });
-
-
-
-
-}
+};
