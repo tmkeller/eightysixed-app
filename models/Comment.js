91200cd0dbdc5ab9c6e735a37231e83f97cab1c9
Kevin Lyons <zinckev@hotmail.com>
2021-02-02 15:46:44 -0800

    cleaned models and commented functions

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,11 @@
+// exports the module of the table structure
 module.exports = function (sequelize, DataTypes) {
   const Comment = sequelize.define("Comment", {
     title: { type: DataTypes.STRING, allowNull: false },
     body: { type: DataTypes.TEXT, allowNull: false },
   });
 
+  // ties the comment with a foreign key to Reviews table and Business table
   Comment.associate = function (models) {
     Comment.belongsTo(models.Review, { onDelete: "cascade" });
     Comment.belongsTo(models.Customer, { onDelete: "cascade" });
