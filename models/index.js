e7b1a047c208d0368def808476a6b055117555af
Shea Mullaney <seagda@gmail.com>
2021-02-03 09:53:32 -0800

    env_variables

--- models/index.js
+++ models/index.js
@@ -11,16 +11,21 @@ const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL)  
+} else { 
+
+    if (config.use_env_variable) {
+      sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    } else {
+      sequelize = new Sequelize(
+        config.database,
+        config.username,
+        config.password,
+        config
+      );
+    }
+  };
 
 fs.readdirSync(__dirname)
   .filter((file) => {
